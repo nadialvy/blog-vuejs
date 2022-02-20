664b9fd55ad98866e1b717c1b9171604f7866c7b
nadialvy <nadia_lovely_29rpl@smktelkom-malang.sch.id>
2022-02-20 13:07:09 +0700

    "Create a simple blog, applying data binding in vuejs"

---
+++ src/components/addBlog.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="add-blog">
+
+    <h2>Add a New Blog Post</h2>
+
+    <form action="">
+        <label for="">Blog Title</label>
+        <input type="text" required v-model.lazy="blog.title">
+
+        <label for="">Blog Content</label>
+        <textarea v-model.lazy="blog.content"></textarea>
+
+        <div id="checkboxes">
+            <label for="ninja">Ninjas</label>
+            <input id="ninja" type="checkbox" value="ninja" v-model="blog.categories">
+            <label for="wizard">Wizard</label>
+            <input id="wizard" type="checkbox" value="wizard" v-model="blog.categories">
+            <label for="mario">Mario</label>
+            <input id="mario" type="checkbox" value="mario" v-model="blog.categories">
+            <label for="cheese">Cheese</label>
+            <input id="cheese" type="checkbox" value="cheese" v-model="blog.categories">
+        </div>
+    </form>
+
+    <div id="preview">
+        <h3>Preview Blog</h3>
+        <p>Blog title: {{ blog.title }} </p>
+
+        <p>Blog content:  </p>
+        <p>{{ blog.content }}</p>
+
+        <p>Blog Categories: </p>
+        <ul>
+            <li v-for="(category, idx) in blog.categories" v-bind:key="idx">
+                {{ category }}
+            </li>
+        </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+        blog: {
+            title: '',
+            content: '',
+            categories: []
+        }
+    }
+  }
+}
+</script>
+
+<style scoped>
+#add-blog *{
+    box-sizing: border-box;
+}
+
+#add-blog{
+    margin: 20px auto;
+    max-width: 600px;
+    /* border: 1px solid green; */
+}
+
+label{
+    width: 500px;
+    display: block;
+    margin: 20px 0 10px;
+}
+
+input[type="text"], textarea{
+    display: block;
+    width: 600px;
+    padding: 8px;
+}
+
+#preview{
+    padding: 10px 20px;
+    border: 1px dotted #ccc;
+    margin: 30px 0;
+}
+
+h3{
+    margin-top: 10px;
+}
+
+#checkboxes{
+    margin-top: 30px;
+}
+
+#checkboxes label{
+    display: inline;
+}
+
+#checkboxes input{
+    display: inline;
+    margin-right: 15px;
+}
+
+
+</style>
+
